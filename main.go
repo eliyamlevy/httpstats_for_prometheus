5c12b6e9dc91cd40f1efef388c9e62438807a02a
theskellington <yami@eliyam.com>
2018-05-29 12:59:13 -0400

    Added more logging and made the script check the path for the config file. Fixed tcp and dns timers so they actually work now.

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
 	"time"
 
 	prometheus "github.com/prometheus/client_golang/prometheus"
@@ -10,7 +13,24 @@ import (
 	"github.com/tkanos/gonfig"
 )
 
+var confPaths = []string{
+	"config.json",                    //test run
+	"/etc/goscraper/app/config.json", //inside a dockprom setup
+	"/app/config.json",               //inside a docker container
+}
+
 func main() {
+	//Capture if the program is exited and logs exit signal
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		select {
+		case sig := <-c:
+			log.Printf("Got %s signal. Aborting...\n", sig)
+			os.Exit(1)
+		}
+	}()
+
 	//Create config struct
 	type Configuration struct {
 		Port   string
@@ -19,14 +39,19 @@ func main() {
 	configuration := Configuration{}
 
 	//Grab Details
-	err := gonfig.GetConf("config.json", &configuration)
-	if err != nil {
-		log.Printf("error: %v", err)
+	for i := 0; i < len(confPaths); i++ {
+		err := gonfig.GetConf(confPaths[i], &configuration)
+		if err != nil {
+			log.Printf("error: %v", err)
+		} else if err == nil {
+			log.Printf("config file read from " + confPaths[i])
+		}
 	}
 
 	//Create Client
 	client := http.DefaultClient
 	client.Timeout = 1 * time.Second
+	log.Printf("client created")
 
 	//Create Summaries
 	// dnsLatencyVec
@@ -59,26 +84,22 @@ func main() {
 		},
 		[]string{"method"},
 	)
-
 	//Register all the metrics with Prometheus
 	prometheus.MustRegister(dnsLatencyVec, tcpLatencyVec, requestTimer, requestAvgDurationVec)
-	//
+
+	//Run roundtrip for each URL
 	numURLs := len(configuration.URLmap)
 	for i := 0; i < numURLs; i++ {
 		// Define functions for the available httptrace.ClientTrace hook
 		// functions that we want to instrument.
+		timer1 := prometheus.NewTimer(prometheus.ObserverFunc(dnsLatencyVec.WithLabelValues("dns_start", configuration.URLmap[i]).Set))
+		timer2 := prometheus.NewTimer(prometheus.ObserverFunc(tcpLatencyVec.WithLabelValues("TCP_start", configuration.URLmap[i]).Set))
 		trace := &promhttp.InstrumentTrace{
-			DNSStart: func(t float64) {
-				dnsLatencyVec.WithLabelValues("dns_start", configuration.URLmap[i])
-			},
 			DNSDone: func(t float64) {
-				dnsLatencyVec.WithLabelValues("dns_done", configuration.URLmap[i])
+				timer1.ObserveDuration()
 			},
-			GotConn: func(t float64) {
-				tcpLatencyVec.WithLabelValues("TCP_start", configuration.URLmap[i])
-			},
-			GotFirstResponseByte: func(t float64) {
-				tcpLatencyVec.WithLabelValues("TCP_done", configuration.URLmap[i])
+			ConnectDone: func(t float64) {
+				timer2.ObserveDuration()
 			},
 		}
 
@@ -86,9 +107,10 @@ func main() {
 		roundTripper := promhttp.InstrumentRoundTripperTrace(trace,
 			promhttp.InstrumentRoundTripperDuration(requestAvgDurationVec, http.DefaultTransport))
 
-		timer := prometheus.NewTimer(prometheus.ObserverFunc(requestTimer.WithLabelValues("GET", configuration.URLmap[i]).Set))
+		timer0 := prometheus.NewTimer(prometheus.ObserverFunc(requestTimer.WithLabelValues("GET", configuration.URLmap[i]).Set))
 		// Set the RoundTripper on the client.
 		client.Transport = roundTripper
+		log.Printf("roundtripper created")
 
 		//Read and close body for each url
 		resp, err := client.Get(configuration.URLmap[i])
@@ -96,9 +118,11 @@ func main() {
 			log.Printf("error: %v", err)
 		}
 		resp.Body.Close()
-		timer.ObserveDuration()
+		timer0.ObserveDuration()
+		log.Printf("GET run with url: " + configuration.URLmap[i])
 	}
-	log.Printf("%+v", configuration)
+
+	log.Printf("Config: " + fmt.Sprintf("%+v", configuration))
 	// The Handler function provides a default handler to expose metrics
 	// via an HTTP server. "/metrics" is the usual endpoint for that.
 	http.Handle("/metrics", promhttp.Handler())
